b7f910f1eb20e7fcab71b2adb80d497dc12a9002
Griffin Beronio <griffinberonio@gmail.com>
2026-04-26 14:46:10 -0500

    Submitting changes to tables, adding tables for RF feature importances

--- Practicum_Generator_Mapping.R
+++ Practicum_Generator_Mapping.R
@@ -49,17 +49,32 @@ cook_solar %>% head()
 tracts = tracts(state='IL', county = 'Cook')
 tracts %>% summarize('AWATER') 
 
+  #Deleting large lake tract by area
+tracts <- tracts %>% mutate('AREA' = as.numeric(st_area(tracts)))
+maxarea <- max(tracts$AREA)
+tractsclean <- tracts %>% subset(AREA < maxarea)
+tractsclean %>% head()
 
-tracts %>% head()
 
-tractsandsolar <- tracts %>% st_join(tracts, cook_solar)
+cook_solar %>% colnames()
+cook_solar$category %>% unique()
 
-# Mapping Plant IDs for Time Period 1: ------------------------------------
+tractsandsolar <- tractsclean %>% inner_join(cook_solar, by='GEOID')
+tractsandsolar <- tractsandsolar %>% drop_na(YEAR)
+minyear <- min(tractsandsolar$YEAR)
+maxyear <- max(tractsandsolar$YEAR)
+minyear
+maxyear
 
-#County area Basemap: 
+tractsandsolar %>% head()
+tractsandsolar %>%
+  st_drop_geometry() %>%
+  count(GEOID) %>%
+  summary()
 
+# County area Basemap: 
 cook <- "Cook"
-  
+
 il_counties <- counties(state = "IL", cb = TRUE, class = "sf")
 il_counties$NAME
 # il_counties %>% head(0)
@@ -69,21 +84,215 @@ il_counties %>% select(COUNTY)
 il_counties %>% colnames
 cook_county <- il_counties %>% filter(COUNTY == cook) %>% 
   select(COUNTY)
-cook_county 
-il_counties %>% colnames()
 
-ggplot() +
+# Multiple DFs per year snapshot:
+
+#2019
+year2019 <- tractsandsolar %>% filter(YEAR == 2019)
+year2019 %>% dim()
+gencounts2019 <- year2019 %>% 
+  group_by(GEOID,geometry) %>% 
+  summarize('GENCOUNTS' = n(), .groups = 'drop') %>% 
+  st_as_sf() %>% 
+  mutate(YEAR = 2019)
+
+#2022
+year2022 <- tractsandsolar %>% filter(YEAR == 2022)
+year2022 %>% dim()
+gencounts2022 <- year2022 %>% 
+  group_by(GEOID,geometry) %>% 
+  summarize('GENCOUNTS' = n(), .groups = 'drop') %>% 
+  st_as_sf() %>% 
+  mutate(YEAR = 2022)
+
+#2025
+year2025 <- tractsandsolar %>% filter(YEAR == 2025)
+year2025 %>% dim()
+gencounts2025 <- year2025 %>% 
+  group_by(GEOID,geometry) %>% 
+  summarize('GENCOUNTS' = n(), .groups = 'drop') %>% 
+  st_as_sf() %>% 
+  mutate(YEAR = 2025)
+
+# --- 2. Get shared min/max across all three years ---
+all_counts <- c(gencounts2019$GENCOUNTS, gencounts2022$GENCOUNTS, gencounts2025$GENCOUNTS)
+scale_min <- min(all_counts, na.rm = TRUE)
+scale_max <- max(all_counts, na.rm = TRUE)
+
+# Mapping Generators for all years with a function: ------------------------------------
+map_year <- 'none'
+map_year <- function(df, year) {
+  ggplot(df) +
+    geom_sf(data=cook_county, color = 'black') +
+    geom_sf(data=tractsclean, color='black')+
+    geom_sf(aes(fill = GENCOUNTS), color = "white", linewidth = 0.1) +
+    scale_fill_viridis_c(
+      option = "plasma",
+      name = "Generators",
+      limits = c(scale_min, scale_max)  # shared scale
+    ) +
+    labs(subtitle = year) +
+    theme_void() +
+    theme(
+      plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
+      legend.position = "right"
+    )
+}
+
+# Calling the map function:
+map2019 <- map_year(gencounts2019, 2019)
+map2022 <- map_year(gencounts2022, 2022)
+map2025 <- map_year(gencounts2025, 2025)
+
+#Side-by-side Display:
+library(patchwork)
+all_years <- map2019 + map2022 + map2025 + 
+  plot_layout(guides = "collect") +  # merges the three legends into one
+  plot_annotation(
+    title = "Generators per Census Tract by Year",
+    theme = theme(plot.title = element_text(hjust = 0.5, face = "bold", size = 16))
+  )
+
+all_years
+
+
+  #Number of generators
+year2019 <- tractsandsolar %>% filter(YEAR == 2019)
+year2019 %>% dim()
+gencounts2019 <- year2019 %>% 
+  group_by(GEOID,geometry) %>% 
+  summarize('GENCOUNTS' = n(), .groups = 'drop') %>% 
+  st_as_sf() 
+gencounts2019 %>% summary()
+
+#Data sources:
+year2019 %>% colnames()
+year2019 %>% group_by(source_file) %>% summarize(n = n())
+year2019$source_file %>% unique()
+
+# Mapping:
+full_map_2019 <- ggplot(gencounts2019) +
   geom_sf(data=cook_county, color = 'black') +
-  geom_sf(data=tracts, color='black')+
-  # scale_fill_gradient(low='lightblue', high='darkmagenta', na.value = 'grey90') +
-  theme_minimal() +
-  labs(title = "Cook County")
+  geom_sf(data=tractsclean, color='black')+
+  geom_sf(aes(fill = GENCOUNTS), color = "skyblue", linewidth = 0.1) +
+  scale_fill_viridis_c(
+    option = "plasma",
+    name = "Generators",
+    limits = c(scale_min, scale_max),
+    na.value = "grey80",
+  ) +
+  labs(
+    title = paste("Generators per Census Tract (2019)"),
+    caption = "Source: IPA Illinois Shines Block Grant Applications"
+  ) +
+  theme_void() +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
+    legend.position = "right"
+  )
+
+full_map_2019
+
+  #Capacity 
+capcounts2019 <- year2019 %>% 
+  group_by('GEOID') %>% 
+  summarize('CAPACITY' = sum(kw))
+
+
 
 
 # Mapping Plant IDs for Time Period 2: ------------------------------------
 
+year2022 <- tractsandsolar %>% filter(YEAR == 2022)
+year2022 %>% dim()
+gencounts2022 <- year2022 %>% 
+  group_by(GEOID,geometry) %>% 
+  summarize('GENCOUNTS' = n(), .groups = 'drop') %>% 
+  st_as_sf() 
+gencounts2022 %>% summary()
+
+#Data sources:
+year2022 %>% group_by(source_file) %>% summarize(n = n())
+year2022$source_file %>% unique()
+
+# Mapping:
+full_map_2022 <- ggplot(gencounts2022) +
+  geom_sf(data=cook_county, color = 'black') +
+  geom_sf(data=tractsclean, color='black')+
+  geom_sf(aes(fill = GENCOUNTS), color = "skyblue", linewidth = 0.1) +
+  scale_fill_viridis_c(
+    option = "plasma",
+    name = "Generators",
+    limits = c(scale_min, scale_max),
+    na.value = "grey80"
+  ) +
+  labs(
+    title = paste("Generators per Census Tract (2022)"),
+    caption = "Source: IPA Illinois Shines Block Grant & Solar For All Applications"
+  ) +
+  theme_void() +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
+    legend.position = "right"
+  )
+
+full_map_2022
+
+
+
 # Mapping Plant IDs for Time Period 3: ------------------------------------
 
+year2025 <- tractsandsolar %>% filter(YEAR == 2025)
+year2025 %>% dim()
+gencounts2025 <- year2025 %>% 
+  group_by(GEOID,geometry) %>% 
+  summarize('GENCOUNTS' = n(), .groups = 'drop') %>% 
+  st_as_sf() 
+gencounts2025 %>% summary()
+
+#Data sources:
+year2025 %>% group_by(source_file) %>% summarize(n = n())
+year2025$source_file %>% unique()
+
+# Mapping:
+full_map_2025 <- ggplot(gencounts2025) +
+  geom_sf(data=cook_county, color = 'black') +
+  geom_sf(data=tractsclean, color='black')+
+  geom_sf(aes(fill = GENCOUNTS), color = "skyblue", linewidth = 0.1) +
+  scale_fill_viridis_c(
+    option = "plasma",
+    name = "Generators",
+    limits = c(scale_min, scale_max),
+    na.value = "grey80"
+  ) +
+  labs(
+    title = paste("Generators per Census Tract (2025)"),
+    caption = "Source: IPA Illinois Shines Block Grant & Solar For All Applications"
+  ) +
+  theme_void() +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
+    legend.position = "right"
+  )
+
+full_map_2025
+
+# Cook County Basemap with Census Tracts:
+
+ggplot() +
+  geom_sf(data=cook_county, color = 'black') +
+  geom_sf(data=tractsclean, color='black')+
+  # scale_fill_gradient(low='lightblue', high='darkmagenta', na.value = 'grey90') +
+  theme_void() +
+  labs(title = "Cook County")
+
+
+
+# Saving Maps to directory: ------------------------------------
+
+directory <- '/Users/griffinberonio/Documents/AAE 724/genmaps'
+full_map_2019
+path <- paste(directory,'2019_full')
 
 
 
